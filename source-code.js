74d9897077c55053b6b8ca4e6dfaf88d77284b7d
Kevo <39010496+CWKevo@users.noreply.github.com>
2020-08-21 11:37:36 +0200

    Update source-code.js

--- source-code.js
+++ source-code.js
@@ -23,6 +23,30 @@ const { CommentStream } = require("snoostorm");
 
 const request = require("request");
 
+/* Fetch the knowledge base: */
+  let knowledgeBase;
+
+  async function getJSON(url, callback) {
+    request(
+      {
+        url: url,
+        json: true
+      },
+      function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+          callback(body);
+        }
+      }
+    );
+  }
+
+  getJSON(
+    "http://raw.githubusercontent.com/CWKevo/Reddit-DoAnAnalysis/master/daa-knowledge-base.json",
+    async function(body) {
+      knowledgeBase = body;
+    }
+  );
+
 const Snoowrap = require("snoowrap");
 const Snoostorm = require("snoostorm");
 
@@ -45,103 +69,59 @@ const stream = new CommentStream(r, {
 
 /* Every time a comment gets added on subreddit: */
 stream.on("item", async comment => {
-  if (comment.created_utc < awoken) return;
+  if ((await comment.created_utc) < awoken) return;
   /* Proceed only if the bot is mentioned: */
 
   if ((await comment.body) == "u/DoAnAnalysis") {
-    /* Fetch the knowledge base: */
-    let knowledgeBase;
-
-    function getJSON(url, callback) {
-      request(
-        {
-          url: url,
-          json: true
-        },
-        function(error, response, body) {
-          if (!error && response.statusCode === 200) {
-            callback(body);
-          }
-        }
-      );
-    }
-
-    getJSON(
-      "http://raw.githubusercontent.com/CWKevo/Reddit-DoAnAnalysis/master/daa-knowledge-base.json",
-      async function(body) {
-        knowledgeBase = body;
-      }
-    );
-    console.log("I was mentioned.");
+    await console.log("I was mentioned.");
     /* If the subreddit is located in the bot's knowledge base... */
     if (await knowledgeBase["subreddits"][comment.subreddit_name_prefixed]) {
-      console.log(
+      await console.log(
         comment.subreddit_name_prefixed +
           " is in my database. Sending reply (" +
           knowledgeBase["subreddits"][comment.subreddit_name_prefixed] +
           ")"
       );
       /* ...reply with the subreddit information from the retrived JSON: */
-      comment.reply(
+      await comment.reply(
         knowledgeBase["subreddits"][comment.subreddit_name_prefixed] +
           "\n\n^(I am a bot and this action was performed automatically | Check our) [^(GitHub repository)](https://github.com/CWKevo/Reddit-DoAnAnalysis) ^(to add your own descriptions to other subreddits.)"
       );
-    } else if (!knowledgeBase["subreddits"][comment.subreddit_name_prefixed]) {
-      console.log(
+    } else if (
+      await !knowledgeBase["subreddits"][comment.subreddit_name_prefixed]
+    ) {
+      await console.log(
         comment.subreddit_name_prefixed +
           " is not in my database. Consider adding it to support more subreddits."
       );
-      comment.reply(
-        "This subreddit is not in my database yet.\n\n^(Perhaps you can help adding a description for this subreddit? Check our) [^(GitHub repository)](https://github.com/CW_Kevo/Reddit-DoAnAnalysis) ^(| Love? Hate?) [^(Send feedback)](https://www.reddit.com/r/DoAnAnalysis)^(!)"
+      await comment.reply(
+        "This subreddit is not in my database yet.\n\n^(Perhaps you can help adding a description for this subreddit? Check our) [^(GitHub repository)](https://github.com/CWKevo/Reddit-DoAnAnalysis) ^(| Love? Hate?) [^(Send feedback)](https://www.reddit.com/r/DoAnAnalysis)^(!)"
       );
     }
   }
 
   // This one is maybe taken way too far:
   if ((await comment.body) == "u/DoAnAnalysis author") {
-    /* Fetch the knowledge base: */
-    let knowledgeBase;
-
-    function getJSON(url, callback) {
-      request(
-        {
-          url: url,
-          json: true
-        },
-        function(error, response, body) {
-          if (!error && response.statusCode === 200) {
-            callback(body);
-          }
-        }
-      );
-    }
-
-    getJSON(
-      "http://raw.githubusercontent.com/CWKevo/Reddit-DoAnAnalysis/master/daa-knowledge-base.json",
-      async function(body) {
-        knowledgeBase = body;
-      }
-    );
     if (await knowledgeBase["users"]["u/" + comment.link_author]) {
-      console.log(
+      await console.log(
         comment.link_author +
           " is in my database. Sending reply (" +
           knowledgeBase["users"]["u/" + comment.link_author] +
           ")"
       );
       /* ...reply with the user information from the retrived JSON: */
-      comment.reply(
+      await comment.reply(
         knowledgeBase["users"]["u/" + comment.link_author] +
           "\n\n^(I am a bot and this action was performed automatically | Check our) [^(GitHub repository)](https://github.com/CWKevo/Reddit-DoAnAnalysis) ^(to add your own descriptions to other subreddits.)"
       );
-    } else if (!knowledgeBase["users"]["u/" + comment.link_author]) {
-      console.log(
+    } else if (await !knowledgeBase["users"]["u/" + comment.link_author]) {
+      await console.log(
         "u/" +
           comment.link_author +
           " is not in my database. Consider adding it to support more users."
       );
-      comment.reply(
-        "This user is not in my database yet.\n\n^(Perhaps you can help adding a description for this user? Check our) [^(GitHub repository)](https://github.com/CW_Kevo/Reddit-DoAnAnalysis) ^(| Love? Hate?) [^(Send feedback)](https://www.reddit.com/r/DoAnAnalysis)^(!)"
+      await comment.reply(
+        "This user is not in my database yet.\n\n^(Perhaps you can help adding a description for this user? Check our) [^(GitHub repository)](https://github.com/CWKevo/Reddit-DoAnAnalysis) ^(| Love? Hate?) [^(Send feedback)](https://www.reddit.com/r/DoAnAnalysis)^(!)"
       );
     }
   }
